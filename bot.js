b3d69f82f65b1de6f6644f22afea5dde2d1972ac
Hiteke <38786753+Hiteke@users.noreply.github.com>
2019-05-30 22:43:01 +0900

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const generator = require('generate-password');
+
+client.on('ready', () => {
+  var i = 0;
+    while (i<1000) { //Number of messages
+var password = generator.generate({
+    length: 16,
+    numbers: true
+    });
+ client.channels.get('Id').send("https://discord.gift/" + password); //Channel ID
+ i++;
+}
+ });
+ 
+client.login('token'); //Bot Token 
